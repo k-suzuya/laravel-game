f6815114d8b07f621cc30b725d1ddef25fdd6835
k-suzuya <game.project.sk@gmail.com>
2021-05-09 15:52:38 +0900

    .gitignore項目追加

--- public/js/app.js
+++ public/js/app.js
@@ -60723,8 +60723,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/kor/Desktop/git_game/laravel-game_verification/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/kor/Desktop/git_game/laravel-game_verification/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/kor/Desktop/git_game/laravel-game/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/kor/Desktop/git_game/laravel-game/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
